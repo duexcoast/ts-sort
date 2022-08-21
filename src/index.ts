2ac8182e9dcd693b1f691c4fc4580e55a0a48efa
conor.ney <conor.ux@gmail.com>
2022-08-21 00:13:08 -0400

    sort for array of numbers works

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hello');
+class Sorter {
+  // collection: number[];
+  // all these extra lines are unneccsary if we put (public collection: number[]) in the constructor arg
+  constructor(public collection: number[]) {
+    // this.collection = collection;
+  }
+
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -2, 4, 8]);
+sorter.sort();
+
+console.log(sorter.collection);

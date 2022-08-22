8e42b4a7e4906cd612a571ae8f36f0121f8fa81b
conor.ney <conor.ux@gmail.com>
2022-08-21 22:49:50 -0400

    set up for sorting strings refactor

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,8 @@ const sorter = new Sorter([10, 3, -2, 4, 8]);
 sorter.sort();
 
 console.log(sorter.collection);
+
+// to get this to work with strings:
+// strings are immutable.
+// we'd want to split the string into a new array.
+//  we'd also want to turn everything to the same case

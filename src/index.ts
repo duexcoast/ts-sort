1e54d8a91fef44231816c449ac9943ea81989a13
conor.ney <conor.ux@gmail.com>
2022-08-21 23:35:51 -0400

    creating separate classes for sorter and different types of collections

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,4 @@
-class Sorter {
-  // collection: number[];
-  // all these extra lines are unneccsary if we put (public collection: number[]) in the constructor arg
-  constructor(public collection: number[]) {
-    // this.collection = collection;
-  }
-
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
+import { Sorter } from './Sorter';
 
 const sorter = new Sorter([10, 3, -2, 4, 8]);
 sorter.sort();

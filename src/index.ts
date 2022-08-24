254edf0f5382e90ba7aee4e3e821b602e47f8a9b
conor.ney <conor.ux@gmail.com>
2022-08-23 23:11:11 -0400

    added class for sorting strings, implements Sortable interface

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
-const numbersCollection = new NumbersCollection([10, 3, -2, 4, 8]);
-const sorter = new Sorter(numbersCollection);
+import { CharactersCollection } from './CharactersCollection';
+// const numbersCollection = new NumbersCollection([10, 3, -2, 4, 8]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-
-console.log(numbersCollection.data);
-
-// to get this to work with strings:
-// strings are immutable.
-// we'd want to split the string into a new array.
-//  we'd also want to turn everything to the same case
+console.log(charactersCollection.data);
